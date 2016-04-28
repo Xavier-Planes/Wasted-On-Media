0e6fdef65b6032a89b1492d25c4e285a4b23286c
Xavier-Planes <xplanes123@gmail.com>
2016-04-28 19:40:57 +0200

    Update urls.py

--- final/wastedonmedia/urls.py
+++ final/wastedonmedia/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, UpdateView
 from models import *
 from views import *
 
